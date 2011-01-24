c2e581e7a68126da435da382e2e7b695e28e63ae
webjay <jacob@saxberg.dk>
2011-01-24 15:26:00 +0000

    Fixes and possible to refresh feed

--- Twilter.php
+++ Twilter.php
@@ -17,13 +17,16 @@
  * Git: https://github.com/webjay/Twilter
  * 
  * @todo Handle stream errors after connection.
- * @todo Handle asynchronous stream connections.
+ * @todo Use PCNTL for multiple connections.
  *
  */
 
 	abstract class Twilter {
 
 		const STREAM_URL = 'https://stream.twitter.com/1/statuses/filter.json';
+		
+		public $access_token;
+		public $access_secret;
 
 		private $consumer_key = null;
 		private $consumer_secret = null;
@@ -32,10 +35,16 @@
 		private $connectFailures = 0;
 		protected $connectFailuresMax = 20; 
 		const TCP_BACKOFF_MAX = 16;
+		
+		public $refresh = false;
+		const REFRESH_INTERVAL = 10;
+		private $refresh_checked;
+		private $connect = true;
 
 		const EARTH_RADIUS_KM = 6371;
 		private $locationBoxes = array();
 		private $track = array();
+		public $count = 0;
 
 		private $options = array(
 			'http' => array(
@@ -43,12 +52,12 @@
 				'follow_location' => 1,
 				'max_redirects' => 5,
 				'protocol_version' => 1.0,
-				'timeout' => 8.0,
+				'timeout' => 5.0,
 				'ignore_errors' => false,
 				'user_agent' => 'Twilter 1.0'
 			)
 		);
-		
+
 		private $stream;
 		private $parameters = array();
 		
@@ -86,6 +95,7 @@
 			if (!empty($this->locationBoxes)) {
 				$this->parameters['locations'] = implode(',', $this->locationBoxes);
 			}
+			//$this->parameters['count'] = $this->count;
 			return $this->parameters;
 		}
 		
@@ -93,61 +103,102 @@
 		 * Connect to Twitter.
 		 * @param string $access_token The users access token provided by Twitter.
 		 * @param string $access_secret The users access secret provided by Twitter.
+		 * @return bool true if connected, false otherwise
 		 */
-		final public function connect ($access_token, $access_secret) {
+		final public function connect () {
+			
+			$this->refresh = false;
+			
+			// init oauth
 			$consumer = new OAuthConsumer($this->consumer_key, $this->consumer_secret);
-			$token = new OAuthConsumer($access_token, $access_secret);
+			$token = new OAuthConsumer($this->access_token, $this->access_secret);
 			$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
 
-			$oauth_url_parameters = $this->get_parameters();
-			$acc_req = OAuthRequest::from_consumer_and_token($consumer, $token, 'POST', $this->create_url(), $oauth_url_parameters);
-			$acc_req->sign_request($hmac_method, $consumer, $token);
-			
+			// set stream options
 			$context = stream_context_create($this->options);
+			stream_context_set_params($context, array('notification' => array($this, 'stream_notification_callback')));
 
-			$this->stream = fopen($acc_req->to_url(), 'r', false, $context);
-			if ($this->stream === false) {
-				// Some sort of socket error has occured
-				$lastErrorNo = is_resource($this->stream) ? socket_last_error($this->stream) : NULL;
-				$lastErrorMsg = ($lastErrorNo > 0) ? socket_strerror($lastErrorNo) : 'Socket disconnected';
-				if (!$lastErrorNo) {
-					$error = error_get_last();
-					$lastErrorMsg = $error['message'];
+			// try connecting for as long as we may
+			do {
+				// make oauth url
+				$req = OAuthRequest::from_consumer_and_token($consumer, $token, 'POST', $this->create_url(), $this->get_parameters());
+				$req->sign_request($hmac_method, $consumer, $token);
+				// connect
+				$this->stream = fopen($req->to_url(), 'r', false, $context);
+				// error?
+				if ($this->stream === false) {
+					// Some sort of socket error has occured
+					$lastErrorNo = is_resource($this->stream) ? socket_last_error($this->stream) : NULL;
+					$lastErrorMsg = ($lastErrorNo > 0) ? socket_strerror($lastErrorNo) : 'Socket disconnected';
+					if (!$lastErrorNo) {
+						$error = error_get_last();
+						$lastErrorNo = $error['type'];
+						$lastErrorMsg = $error['message'];
+					}
+					if (strpos($lastErrorMsg, 'Connection timed out')) {
+						$this->log('Connection timed out');
+					} else {
+						$this->log($lastErrorNo.': '.$lastErrorMsg);
+					}
 				}
-				$this->log($lastErrorMsg);
-				if ($this->sleep()) {
-					$this->connect($access_token, $access_secret);
-				}
-				return false;
-			}
-			stream_set_blocking($this->stream, 1);
-			$this->log(print_r(stream_get_meta_data($this->stream), true), 2);
+			} while ($this->connect && $this->stream === false && $this->sleep());
+			
+			stream_set_blocking($this->stream, 0);
+
 			// reset connect failures
 			$this->connectFailures = 0;
+			$this->tcpSleep = 1;
+
+			$this->log(print_r(stream_get_meta_data($this->stream), true), 2);
+			
+			return true;
+
 		}
-		
+
 		/**
 		 * Start receiving the stream.
 		 * Output is sent to output.
 		 * @see Twilter::output
 		 */
 		final public function stream_start () {
-			while (!feof($this->stream)) { 
+			$this->log('Stream started');
+			while (feof($this->stream) !== true && $this->refresh === false && $this->connect) { 
 				$this->output(stream_get_line($this->stream, 65535));
+				if ($this->refresh_checked < time() - self::REFRESH_INTERVAL) {
+					$this->refresh_checked = time();
+					$this->refresh();
+				}
 			}
 			fclose($this->stream);
+			if ($this->refresh) {
+				$this->connect();
+				$this->stream_start();
+			}
 		}
 
 		/**
-		 * Overwrite me
-		 * @param string $str Output from Twitter.
+		 * Output from the stream. Overwrite me
+		 * @param string $str JSON output from Twitter.
 		 */
 		abstract protected function output ($str);
+
+		/**
+		 * Check for data refresh
+		 */
+		abstract public function refresh ();
+
+		/**
+		 * Logging. Overwrite me
+		 * @param string $msg debug or error message
+		 * @param integer $level debug level
+		 */
+		abstract protected function log ($msg, $level = 1);
 		
 		final private function create_url () {
 			$urlParts = parse_url(self::STREAM_URL);
 			$url = $urlParts['scheme'].'://';
 			$url .= $this->get_random_ip($urlParts['host']);
+			$url .= ($urlParts['scheme'] == 'https') ? ':443' : ':80';
 			$url .= $urlParts['path'];
 			return $url;
 		}
@@ -157,7 +208,7 @@
 			if (count($streamIPs) == 0) {
 				throw new ErrorException('Unable to resolve hostname: '.$host);
 			}
-			$this->log('Resolved host '.$host.' to '.implode(', ', $streamIPs));
+			$this->log('Resolved '.$host.' to '.implode(', ', $streamIPs));
 			// Choose one randomly (if more than one)
 			$streamIP = $streamIPs[rand(0, (count($streamIPs) - 1))];
 			$this->log('Connecting to '.$streamIP);
@@ -170,28 +221,18 @@
 				$msg = 'Connection failure limit exceeded with '.$connectFailures.' failures.';
 				$this->log($msg);
 				throw new ErrorException($msg);
+				// no more retries
 				return false;
 			}
 			// Increase retry/backoff up to max
 			$this->tcpSleep = ($this->tcpSleep < self::TCP_BACKOFF_MAX) ? $this->tcpSleep * 2 : self::TCP_BACKOFF_MAX;
+			// log
+			$connectFailuresLeft = $this->connectFailuresMax - $this->connectFailures;
+			$this->log('Sleeping '.$this->tcpSleep.' seconds. '.$connectFailuresLeft.' connection retries left.');
+			// sleep
 			sleep($this->tcpSleep);
+			// retry
 			return true;
-		}
-		
-		/**
-		 * Logging. Overwrite me
-		 * @param string $msg debug or error message
-		 * @param integer $level debug level
-		 */
-		protected function log ($msg, $level = 1) {
-			switch ($level) {
-				case 1:
-					echo($msg.PHP_EOL);
-					break;
-				case 2:
-					echo($msg.PHP_EOL);
-					break;
-			}
 		}
 
 		/**
@@ -248,6 +289,41 @@
 			}
 			// Set by bounding boxes
 			$this->setLocations($boundingBoxes);
+		}
+
+		private function stream_notification_callback ($notification_code, $severity, $message, $message_code, $bytes_transferred, $bytes_max) {
+			switch ($notification_code) {
+				case STREAM_NOTIFY_RESOLVE:
+				case STREAM_NOTIFY_AUTH_REQUIRED:
+				case STREAM_NOTIFY_COMPLETED:
+				case STREAM_NOTIFY_FAILURE:
+				case STREAM_NOTIFY_AUTH_RESULT:
+					$this->log('code:'.$notification_code.', severity:'.$severity.', msg:'.$message.', msgcode:'.$message_code);
+					if ($message_code == 406) {
+						$this->connect = false;
+					}
+					break;
+
+				case STREAM_NOTIFY_REDIRECTED:
+					$this->log('Redirected to: '.$message, 2);
+					break;
+
+				case STREAM_NOTIFY_CONNECT:
+					$this->log('Connected', 1);
+					break;
+
+				case STREAM_NOTIFY_FILE_SIZE_IS:
+					$this->log('Filesize: '.$bytes_max, 3);
+					break;
+
+				case STREAM_NOTIFY_MIME_TYPE_IS:
+					$this->log('MIME type: '.$message, 2);
+					break;
+
+				case STREAM_NOTIFY_PROGRESS:
+					$this->log('Downloaded '.$bytes_transferred.' bytes so far', 3);
+					break;
+			}
 		}
 
 	}
